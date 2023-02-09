73912f92e148366ebab47d027d6a76e3de166034
MayuRuru <aruizrull@protonmail.com>
2023-02-09 19:13:33 +0100

    correct comparing dates

--- src/components/notes/NotesList.js
+++ src/components/notes/NotesList.js
@@ -3,13 +3,14 @@ import "./NotesList.css";
 import { useSelector } from "react-redux";
 import { selectNotesList } from "../../features/notes/notesSlice";
 import NotesAuthor from "../../features/notes/NotesAuthor";
-import TimeConverter from "../../helper/TimeConverter";
+//import TimeConverter from "../../helper/TimeConverter";
 
 const NotesList = () => {
   const notes = useSelector(selectNotesList);
+
   const orderedNotes = notes
     .slice() // it creates a shadow copy of the array
-    .sort((a, b) => b.date.localeCompare(a.date));
+    .sort((a, b) => b.date - a.date);
 
   const renderedNotes = orderedNotes.map((note) => (
     <article key={note.id}>
@@ -17,7 +18,7 @@ const NotesList = () => {
       <p>{note.content.substring(0, 100)}</p>
       <p className="noteCredit">
         <NotesAuthor userId={note.userId} />
-        <TimeConverter timestamp={note.date} />
+        {/* <TimeConverter timestamp={note.date} /> */}
       </p>
     </article>
   ));
