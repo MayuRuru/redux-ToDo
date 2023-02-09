07616e7bd142819aa45a1ab546cd3f6dcbae2b3a
MayuRuru <aruizrull@protonmail.com>
2023-02-09 17:47:52 +0100

    order post by newest first

--- src/components/notes/NotesList.js
+++ src/components/notes/NotesList.js
@@ -7,8 +7,11 @@ import TimeConverter from "../../helper/TimeConverter";
 
 const NotesList = () => {
   const notes = useSelector(selectNotesList);
+  const orderedNotes = notes
+    .slice() // it creates a shadow copy of the array
+    .sort((a, b) => b.date.localeCompare(a.date));
 
-  const renderedNotes = notes.map((note) => (
+  const renderedNotes = orderedNotes.map((note) => (
     <article key={note.id}>
       <h3>{note.title}</h3>
       <p>{note.content.substring(0, 100)}</p>
