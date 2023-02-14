ff7e84c5ad716c7a02aae4b802273f2a65114325
MayuRuru <aruizrull@protonmail.com>
2023-02-14 12:02:42 +0100

    Implement querybuilders

--- src/components/tasks/TasksList.js
+++ src/components/tasks/TasksList.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./TasksList.css";
 import Input from "./Input";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import TaskItem from "./TaskItem";
 import { useSelector } from "react-redux";
 import { selectTaskList } from "../../features/tasks/tasksSlice";
-import { useGetTasksQuery } from "../../features/api/apiSlice";
+import {
+  useGetTasksQuery,
+  useAddTaskMutation,
+  useUpdateTaskMutation,
+  useDeleteTaskMutation,
+} from "../../features/api/apiSlice";
 
 const TaskList = () => {
   //const taskList = useSelector(selectTaskList);
-
   const {
     data: tasks,
     isLoading,
@@ -16,12 +22,51 @@ const TaskList = () => {
     isError,
     error,
   } = useGetTasksQuery();
+  const [addTask] = useAddTaskMutation();
+  const [updateTask] = useUpdateTaskMutation();
+  const [deleteTask] = useDeleteTaskMutation();
+
+  /*   const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask({name:input, done:false});
+    setInput('')
+  } */
 
   let content;
   if (isLoading) {
     content = <p>Loading...</p>;
   } else if (isSuccess) {
-    content = JSON.stringify(tasks);
+    /* content = JSON.stringify(tasks); */
+    content = tasks.map((task) => {
+      return (
+        <article key={task.id}>
+          <div className="task">
+            <input
+              type="checkbox"
+              id={task.id}
+              done={task.done}
+              onChange={() =>
+                updateTask({
+                  ...task,
+                  done: !task.done,
+                })
+              }
+            />
+            <label htmlFor={task.id}>{task.name}</label>
+          </div>
+          <button
+            className="trash"
+            onClick={() =>
+              deleteTask({
+                id: task.id,
+              })
+            }
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      );
+    });
   } else if (isError) {
     content = <p>{error}</p>;
   }
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,14 +5,48 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3500",
   }),
+  // We need to add a tag to specify which mutations
+  // invalidate the previous cached results
+  // so it automatically fetched new data
+  tagTypes: ["Tasks"],
+
   endpoints: (builder) => ({
     getTasks: builder.query({
       query: () => "/tasks",
+      transformErrorResponse: (res) => res.sort((a, b) => b.id - a.id),
+      providesTags: ["Tasks"],
+    }),
+    addTask: builder.mutation({
+      query: (task) => ({
+        url: "/tasks",
+        method: "POST",
+        body: task,
+      }),
+      invalidatesTags: ["Tasks"],
+    }),
+    updateTask: builder.mutation({
+      query: (task) => ({
+        url: `/tasks/${task.id}`,
+        method: "PATCH",
+        body: task,
+      }),
+      invalidatesTags: ["Tasks"],
+    }),
+    deleteTask: builder.mutation({
+      query: ({ id }) => ({
+        url: `/tasks/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
+      invalidatesTags: ["Tasks"],
     }),
   }),
 });
 
 // RTK Query provides custom hooks based on the names of out methods:
 export const {
-    useGetTasksQuery
-} = apiSlice
+  useGetTasksQuery,
+  useAddTaskMutation,
+  useUpdateTaskMutation,
+  useDeleteTaskMutation,
+} = apiSlice;
