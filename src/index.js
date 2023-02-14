a75473b8ae928ed9c21a9eac7bc1ec9a788024d6
MayuRuru <aruizrull@protonmail.com>
2023-02-14 10:55:44 +0100

    Create apiSlice and import as apiProvider

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App";
 import "./index.css";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { apiSlice } from "./features/api/apiSlice";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/*     <Provider store={store}>
       <App />
-    </Provider>
+    </Provider> */}
+
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 );
