17a762b277176f6c5e4eccd7bd1919a7956857c5
MayuRuru <aruizrull@protonmail.com>
2023-02-15 14:25:00 +0100

    Add store.dispatch with tasksApiSlice to index

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import store from "./app/store";
 import App from "./App";
 import "./index.css";
 import { extendedApiSlice } from "./features/notes/notesSlice_extended";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
-import { apiSlice } from "./features/api/apiSlice";
+import { tasksApiSlice } from "./features/tasks/tasksSlice_extended";
+//import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 store.dispatch(extendedApiSlice.endpoints.getNotes.initiate());
+store.dispatch(tasksApiSlice.endpoints.getTasks.initiate());
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-
     {/* <ApiProvider api={apiSlice}>
       <App />
     </ApiProvider> */}
