ef45c9adc398b60a883a379718026def423dab62
MayuRuru <aruizrull@protonmail.com>
2023-02-15 10:35:32 +0100

    Change from ApiProvder to extendedApiSlice

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App";
 import "./index.css";
+import { extendedApiSlice } from "./features/notes/notesSlice_extended";
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import { apiSlice } from "./features/api/apiSlice";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+
+store.dispatch(extendedApiSlice.endpoints.getNotes.initiate());
 
 root.render(
   <React.StrictMode>
