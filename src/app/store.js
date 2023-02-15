c2bb3fdb7e3de94c068b2592f39d1ed579b28e93
MayuRuru <aruizrull@protonmail.com>
2023-02-15 10:36:48 +0100

    Chance notesReducer to apiSliceReducerPath

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,14 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import tasksReducer from "../features/tasks/tasksSlice";
-import notesReducer from "../features/notes/notesSlice";
+//import notesReducer from "../features/notes/notesSlice";
 import usersReducer from "../features/users/usersSlice";
 import { apiSlice } from "../features/api/apiSlice";
 
 export default configureStore({
   reducer: {
     tasks: tasksReducer,
-    //[apiSlice.reducerPath]: apiSlice.reducer,
-    notes: notesReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    //notes: notesReducer,
     users: usersReducer,
   },
   middleware: (getDefaultMiddleware) =>
