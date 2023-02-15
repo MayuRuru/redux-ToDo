a268045e2932d05115dbb9299fd779f6761c9f11
MayuRuru <aruizrull@protonmail.com>
2023-02-15 10:39:29 +0100

    Refact apiSlice to extract endpoints

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,51 +5,6 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3500",
   }),
-  // We need to add a tag to specify which mutations
-  // invalidate the previous cached results
-  // so it automatically fetched new data
-  tagTypes: ["Tasks"],
-
-  endpoints: (builder) => ({
-    getTasks: builder.query({
-      query: () => "/tasks",
-      transformResponse: (res) => res.sort((a, b) => b.id - a.id),
-      providesTags: ["Tasks"],
-    }),
-
-    addTask: builder.mutation({
-      query: (task) => ({
-        url: "/tasks",
-        method: "POST",
-        body: task,
-      }),
-      invalidatesTags: ["Tasks"],
-    }),
-
-    updateTask: builder.mutation({
-      query: (task) => ({
-        url: `/tasks/${task.id}`,
-        method: "PATCH",
-        body: task,
-      }),
-      invalidatesTags: ["Tasks"],
-    }),
-    deleteTask: builder.mutation({
-      query: ({ id }) => ({
-        url: `/tasks/${id}`,
-        method: "DELETE",
-        body: id,
-      }),
-      invalidatesTags: ["Tasks"],
-    }),
-  }),
+  tagTypes: ["Task"],
+  endpoints: (builder) => ({}),
 });
-
-// RTK Query provides custom hooks based on the names of out methods:
-export const {
-  useGetTasksQuery,
-  useAddTaskMutation,
-  useUpdateTaskMutation,
-  useDeleteTaskMutation,
-  // It will be an extendedApiSlice
-} = apiSlice;
